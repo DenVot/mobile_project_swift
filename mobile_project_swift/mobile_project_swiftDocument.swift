0d8f726559219f56604738858c15523bc1a1f246
Denis Vojtenko <vojtenkoda@mts.ru>
2025-09-13 20:04:29 +0300

    Initial Commit

---
+++ mobile_project_swift/mobile_project_swiftDocument.swift
@@ -0,0 +1,39 @@
+//
+//  mobile_project_swiftDocument.swift
+//  mobile_project_swift
+//
+//  Created by Денис Войтенко on 13.09.2025.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var exampleText: UTType {
+        UTType(importedAs: "com.example.plain-text")
+    }
+}
+
+struct mobile_project_swiftDocument: FileDocument {
+    var text: String
+
+    init(text: String = "Hello, world!") {
+        self.text = text
+    }
+
+    static var readableContentTypes: [UTType] { [.exampleText] }
+
+    init(configuration: ReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let string = String(data: data, encoding: .utf8)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        text = string
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = text.data(using: .utf8)!
+        return .init(regularFileWithContents: data)
+    }
+}
