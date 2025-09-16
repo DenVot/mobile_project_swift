896b3cadd129db3c6a0d90972607e9c7d256e8a1
Denis Vojtenko <vojtenkoda@mts.ru>
2025-09-16 20:34:12 +0300

    Fix

---
+++ mobileProjectSwift/mobileProjectSwift/mobileProjectSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileProjectSwiftApp.swift
+//  mobileProjectSwift
+//
+//  Created by Денис Войтенко on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct mobileProjectSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
